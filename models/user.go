1633d62360721e5a3bb35c8113f8233db672119e
Vikash Gupta <mrvikash700@gmail.com>
2026-05-07 20:39:31 +0530

    validations

--- models/user.go
+++ models/user.go
@@ -1,9 +1,11 @@
 package models
 
 type User struct {
-	ID       uint   `json:"id"`
-	Name     string `json:"name"`
-	Email    string `json:"email"`
-	Tasks    []Task `json:"tasks"`
-	Password string `json:"password"`
+	ID       uint   `json:"id" gorm:"primaryKey"`
+	Name     string `json:"name" binding:"required"`
+	Email    string `json:"email" binding:"required,email" gorm:"unique"`
+	Password string `json:"-" binding:"required,min=6"`
+	Age      uint   `json:"age" binding:"required,gte=18"`
+
+	Tasks []Task `json:"tasks" gorm:"foreignKey:UserID"`
 }
